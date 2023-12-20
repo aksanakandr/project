1d7d7ef839e10c90c035f55bca6d26edca7a1e39
aksanakandratovich <aksana.kandratovich@gmail.com>
2023-12-19 19:42:54 -0800

    new tests

--- pythonProject6/tests/test_about_link.py
+++ pythonProject6/tests/test_about_link.py
@@ -5,14 +5,17 @@ from pages.login_page import Login_page
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.main_page import Main_page
-
+from selenium.webdriver.chrome.options import Options
+import pytest
 
 @allure.description("test_link_about")
 def test_about_link(set_up):
+    options = Options()
+    options.add_argumemts('--headless')
     s = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=s)
+    browser = webdriver.Chrome((service=s)(options=options))
 
-    login=Login_page(browser)
+    login = Login_page(browser)
     login.avtorization()
     time.sleep(5)
 
